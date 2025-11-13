6c9dce68642a96840c8bdcaae69ccc3ad8381722
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-13 23:03:16 +0200

    fixed bug

--- schemas.py
+++ schemas.py
@@ -23,8 +23,6 @@ class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
-
-
 class PlainTagSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
@@ -33,6 +31,6 @@ class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     tags = fields.List(fields.Nested(PlainTagSchema), dump_only=True)
 
-class PlainTagSchema(PlainTagSchema):
+class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema, dump_only=True)
