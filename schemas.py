bd8fbbbaae8f161a39ee9fa708cb290dcb2c1179
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-17 14:39:52 +0200

    Fixed schema Bug

--- schemas.py
+++ schemas.py
@@ -35,7 +35,6 @@ class StoreSchema(PlainStoreSchema):
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema, dump_only=True)
-    tags = fields.List(fields.Nested(PlainItemSchema), dump_only=True)
 
 class TagAndItemSchema(Schema):
     message = fields.Str()
