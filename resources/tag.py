63ad5e470dc1f33cfc57e280ae883177a4ad42e5
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-17 13:00:00 +0200

    adding a tag to item

--- resources/tag.py
+++ resources/tag.py
@@ -33,7 +33,22 @@ class TagsInStore(MethodView):
             
         return tag
     
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class LinkTagsToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
 
+        item.tags.append(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+
+        return tag
     
 
 @blp.route("/tag/<string:tag_id>")
