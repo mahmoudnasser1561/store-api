57775cdc405f08366670537aaf23949e02fccb4e
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-17 14:16:29 +0200

    fixed some typos

--- models/item_tags.py
+++ models/item_tags.py
@@ -3,6 +3,6 @@ from db import db
 class ItemTags(db.Model):
     __tableename__ = "items_tags"
 
-    id = db.Column(db.Ineger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
     tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
