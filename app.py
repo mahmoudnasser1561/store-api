6a50db5d82efcd901a9223365195d81c09a0fec8
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-23 22:20:25 +0200

    created register and user get/delete

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 from models import StoreModel
 
@@ -56,5 +57,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
