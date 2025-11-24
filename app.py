3918970842f95f84f28ca0ef580ff480c9e107dd
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-24 16:02:35 +0200

    added JWT Claims

--- app.py
+++ app.py
@@ -53,6 +53,12 @@ def create_app(db_url=None):
     api = Api(app)
 
     jwt = JWTManager(app)
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
