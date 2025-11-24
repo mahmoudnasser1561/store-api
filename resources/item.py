f3ae317b8f4e1372b4938ee60fd5a55c8064c087
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-24 15:51:12 +0200

    Configured JWT Error messages

--- resources/item.py
+++ resources/item.py
@@ -1,11 +1,12 @@
 import uuid
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import  SQLAlchemyError
+from flask_jwt_extended import jwt_required
 
 from schemas import ItemSchema, ItemUpdateSchema
 from models import ItemModel, StoreModel
 from db import db
-from sqlalchemy.exc import  SQLAlchemyError
 
 blp = Blueprint("Items", "items", description="Operations on items")
 
@@ -17,6 +18,7 @@ class Item(MethodView):
         item = ItemModel.query.get_or_404(item_id)
         return item
     
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -24,6 +26,7 @@ class Item(MethodView):
 
         return {"Message": "Item deleted"}
 
+    @jwt_required()
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
@@ -46,6 +49,7 @@ class ItemList(MethodView):
     def get(self):
         return ItemModel.query.all()
 
+    @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
