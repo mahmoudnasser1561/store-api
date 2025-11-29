7097c5f74c3235ecbba9b7b3f9da7b95c5f0a596
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-29 23:09:54 +0200

    user logout feature

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
 
 from models import StoreModel
-
+from blocklist import BLOCKLIST
 from db import db
 
 from dotenv import load_dotenv
@@ -77,6 +77,17 @@ def create_app(db_url=None):
     def unauthorized_loader(error):
         return (
             jsonify({"message" : "Request doesn't contain an access token", "error": "authorization_required"}),
+            401,
+        )
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+    
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message" : "Token has been revoked.", "error": "token_revoked"}),
             401,
         )
 
