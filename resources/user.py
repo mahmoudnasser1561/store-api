e5e4577752b9a9949fce77e2a0781790d54edaa9
Mahmoud <mahmoudnasser1561@gmail.com>
2025-12-02 19:41:20 +0200

    implemented refresh tokens

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt, create_refresh_token, get_jwt_identity
 
 from db import db
 from models import UserModel
@@ -37,10 +37,19 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            access_token = create_access_token(identity=str(user.id))
-            return {"access_token": access_token}
+            access_token = create_access_token(identity=str(user.id), fresh=True)
+            refresh_token = create_refresh_token(identity=str(user.id))
+            return {"access_token": access_token, "refresh_token": refresh_token}
         
         abort(401, message="Invalid credentials.")
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}
 
 @blp.route("/logout")
 class UserLogout(MethodView):
